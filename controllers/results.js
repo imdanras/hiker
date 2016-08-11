e658de760015df94831e1e4531be3b6caf8259d2
Daniel Rasmussen <dan.r.rasmussen@gmail.com>
2016-08-10 18:41:04 -0700

    working on multiple markers on google maps api call

--- controllers/results.js
+++ controllers/results.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res) {
 	request({
 		url: 'https://trailapi-trailapi.p.mashape.com/?q[activities_activity_type_name_eq]=hiking&q[city_cont]=' + req.query.city + '&q[state_cont]=' + req.query.state + '&radius=20',
 		headers: {
-			'X-Mashape-Key': 'ocD09yBCRLmshy5zRdmpPtqwhGINp1qldY3jsngEC2dhf2ljLr',
+			'X-Mashape-Key': process.env.MASHAPE_KEY,
 			'Accept': 'text/plain'
 		}
 	}, function(error, response, body) {
@@ -19,14 +19,23 @@ router.get('/', function(req, res) {
 	})
 })
 
-//AIzaSyAg57CFfCduGwhKOB6ARu6zovvEOGUC36E -- google maps api key
-//   <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAg57CFfCduGwhKOB6ARu6zovvEOGUC36E&callback=initMap"type="text/javascript"></script>
+router.get('/parks', function(req, res) {
+	request({
+		url: 'https://trailapi-trailapi.p.mashape.com/?q[activities_activity_type_name_eq]=hiking&q[city_cont]=' + req.query.city + '&q[state_cont]=' + req.query.state + '&radius=20',
+		headers: {
+			'X-Mashape-Key': process.env.MASHAPE_KEY,
+			'Accept': 'text/plain'
+		}
+	}, function(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var dataObj = JSON.parse(body);
+			
+		}
+	})
 
-// router.get('/:id', function(req, res) {
-// 	request({
-// 		url: ''
-// 	})
-// })
+
+	res.render('parks.ejs');
+});
 
 
 module.exports = router;
